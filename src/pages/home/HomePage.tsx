c7ffb3727fa4af803d2183056016db68f27327e0
Vladislav Hasanov <64597307+PU6ER@users.noreply.github.com>
2024-07-11 18:26:36 +0300

    feat: add home page

---
+++ src/pages/home/HomePage.tsx
@@ -0,0 +1,45 @@
+import { CirclePlus, PencilLine } from 'lucide-react'
+import ChatCard from '../../components/chat-card/ChatCard'
+import { useContacts } from '../../hooks/useContacts'
+import './HomePage.scss'
+
+const words = [
+	{ word: 'apple', translation: 'manzana' },
+	{ word: 'book', translation: 'libro' },
+	{ word: 'car', translation: 'coche' },
+	{ word: 'dog', translation: 'perro' },
+	{ word: 'elephant', translation: 'elefante' },
+]
+const HomePage = () => {
+	const { data } = useContacts('aOKXSmCVqHydL0dbH8xl')
+	console.log(data)
+	return (
+		<div>
+			<div className='topbar'>
+				<div className='topbar-first'>
+					<button className='change'>Изм.</button>
+					<span>Чаты</span>
+					<div className='adjust'>
+						<button>
+							<CirclePlus />
+						</button>
+
+						<button>
+							<PencilLine />
+						</button>
+					</div>
+				</div>
+				<div className=''></div>
+				<div className=''></div>
+				<div className=''></div>
+			</div>
+			<div className='hero'>
+				{[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map(item => (
+					<ChatCard key={item} />
+				))}
+			</div>
+		</div>
+	)
+}
+
+export default HomePage
