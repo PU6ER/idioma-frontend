5b0a3e3535e3c3e472b47774d7422f89ab651bbb
Vladislav Hasanov <64597307+PU6ER@users.noreply.github.com>
2024-07-16 22:46:20 +0300

    feat: add getProfile method

--- src/services/firebase/firebaseService.ts
+++ src/services/firebase/firebaseService.ts
@@ -1,8 +1,17 @@
-import { collection, getDocs, query, where } from 'firebase/firestore'
+import {
+	collection,
+	doc,
+	getDoc,
+	getDocs,
+	query,
+	Timestamp,
+	where,
+} from 'firebase/firestore'
 import { IContacts } from '../../types/contacts.types'
+import { IUser } from '../../types/users.types'
 import { db } from './firebaseConfig'
 
-export async function getContacts(userId: string) {
+export async function getContacts(userId: string): Promise<IContacts[]> {
 	const q = query(collection(db, 'contacts'), where('user_id', '==', userId))
 	try {
 		const docSnapshot = await getDocs(q)
@@ -10,6 +19,41 @@ export async function getContacts(userId: string) {
 			doc => ({ id: doc.id, ...doc.data() } as IContacts)
 		)
 	} catch (error) {
-		return error
+		console.error('Ошибка получения списка контактов', error)
+		return []
+	}
+}
+
+export async function getProfile(userId: string): Promise<IUser> {
+	const docRef = doc(db, 'users', userId)
+	try {
+		const docSnapshot = await getDoc(docRef)
+		if (docSnapshot.exists()) {
+			return { ...docSnapshot.data() } as IUser
+		} else {
+			console.log('No such document!')
+			return {
+				bio: '',
+				created_at: {} as Timestamp,
+				last_seen: {} as Timestamp,
+				display_name: '',
+				email: '',
+				phone_number: '',
+				photo_url: '',
+				username: '',
+			} as IUser
+		}
+	} catch (error) {
+		console.error('Ошибка получения данных о пользователе', error)
+		return {
+			bio: '',
+			created_at: {} as Timestamp,
+			last_seen: {} as Timestamp,
+			display_name: '',
+			email: '',
+			phone_number: '',
+			photo_url: '',
+			username: '',
+		} as IUser
 	}
 }
