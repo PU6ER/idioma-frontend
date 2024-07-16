a3e60ddf721fc39ab8dd4a883968af001c164631
Vladislav Hasanov <64597307+PU6ER@users.noreply.github.com>
2024-07-16 22:46:36 +0300

    feat: add hook for getProfile

---
+++ src/hooks/useProfile.ts
@@ -0,0 +1,10 @@
+import { useQuery } from '@tanstack/react-query'
+import { getProfile } from '../services/firebase/firebaseService'
+
+export function useProfile(userId: string) {
+	const { data, isLoading, isSuccess } = useQuery({
+		queryKey: ['profile'],
+		queryFn: () => getProfile(userId),
+	})
+	return { data, isLoading, isSuccess }
+}
