331b715b8513b7f4ad3afb520e486787851e2a75
Vladislav Hasanov <64597307+PU6ER@users.noreply.github.com>
2024-07-16 22:47:25 +0300

    feat: add new types

--- src/types/contacts.types.ts
+++ src/types/contacts.types.ts
@@ -1,9 +1,6 @@
 export interface IContacts {
-    id: string
+	id: string
 	user_id: string
-	contact: {
-		contact_id: string
-		display_name: string
-		status: string
-	}
+	contact_id: string
+	status: string
 }
---
+++ src/types/users.types.ts
@@ -0,0 +1,12 @@
+import { Timestamp } from 'firebase/firestore'
+
+export interface IUser {
+	display_name: string
+	username: string
+	email: string
+	phone_number: string
+	photo_url: string
+	bio: string
+	created_at: Timestamp
+	last_seen: Timestamp
+}
