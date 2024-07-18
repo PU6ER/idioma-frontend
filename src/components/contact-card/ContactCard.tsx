4a25051f7327b6b80166351d85921228a118b47d
Vladislav Hasanov <64597307+PU6ER@users.noreply.github.com>
2024-07-18 16:04:47 +0300

    feat: add contact card component

--- src/components/contact-card/ContactCard.tsx
+++ src/components/contact-card/ContactCard.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom'
 import { useProfile } from '../../hooks/useProfile'
 import { getLastSeenTime } from '../../services/getLastSeenTime'
 import { IContacts } from '../../types/contacts.types'
@@ -7,10 +8,11 @@ interface IContactParam {
 }
 const ContactCard = ({ contact }: IContactParam) => {
 	const { data, isLoading, isSuccess } = useProfile(contact.contact_id)
-	console.log('contact', contact)
-	data && console.log(data)
+	console.log('contactId', contact.contact_id)
+	console.log('profile ' + data?.id, data)
+
 	return (
-		<div className='contact-card'>
+		<Link to={`/chats/` + data?.id} className='contact-card'>
 			<div className='contact-card__photo'>
 				<img src={data?.photo_url} alt='photo' className='contact-card__img' />
 			</div>
@@ -20,7 +22,7 @@ const ContactCard = ({ contact }: IContactParam) => {
 					{data && getLastSeenTime(data.last_seen)}
 				</span>
 			</div>
-		</div>
+		</Link>
 	)
 }
 
